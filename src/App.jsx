c814ad703840695555dfdf07db58491db02849e1
Pako Cárdenas <pakorro2@gmail.com>
2022-11-21 19:08:34 -0600

    pending changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,94 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import './App.css'
-import UserCard from './components/UserCard'
-import UsersForm from './components/UsersForm'
-import Loading from './components/Loading'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import UserCard from "./components/UserCard";
+import UsersForm from "./components/UsersForm";
+import Loading from "./components/Loading";
 
-
-const baseURL = 'https://users-crud1.herokuapp.com'
+const baseURL = "https://users-crud1.herokuapp.com";
 
 function App() {
   //Estado para guardar la info del usuario
-  const [users, setUsers] = useState()
+  const [users, setUsers] = useState();
   //Estado para guardar la info del usuario obtenida de la Api para editar
-  const [updateInfo, setUpdateInfo] = useState()
-  //Estado que sirve para guardar la clase que oculta el form 
-  const [closeForm, setCloseForm] = useState(true)
+  const [updateInfo, setUpdateInfo] = useState();
+  //Estado que sirve para guardar la clase que oculta el form
+  const [closeForm, setCloseForm] = useState(true);
   //Estado para guardar el estado del loader
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   //Estado que sirve para guardar los errores
-
 
   //Se obtinen todos los usuarios de la api (GET)
   const getAllUsers = () => {
-    const URL = `${baseURL}/users/`
-    setLoading(true)
-    axios.get(URL)
-      .then(res => {
-        setUsers(res.data)
-        setLoading(false)
+    const URL = `${baseURL}/users/`;
+    setLoading(true);
+    axios
+      .get(URL)
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
-  //Controlamos el renderizado de la vista 
+  //Controlamos el renderizado de la vista
   useEffect(() => {
-    getAllUsers()
-  }, [])
+    getAllUsers();
+  }, []);
 
   // Funcion que crea el usuario en la base de datos de la API (POST)
-  const createNewUsers = data => {
-    const URL = `${baseURL}/users/`
-    axios.post(URL, data)
-      .then(res => {
-        console.log(res.data)
+  const createNewUsers = (data) => {
+    const URL = `${baseURL}/users/`;
+    axios
+      .post(URL, data)
+      .then((res) => {
+        console.log(res.data);
         // Actualiza la vista despue de crea el usuaio(getAllUsers)
-        getAllUsers()
-
+        getAllUsers();
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   // Funcion que elimina el usuario en la base de datos de la API (DELETE)
-  const deletedUserById = id => {
-    const URL = `${baseURL}/users/${id}/`
-    axios.delete(URL)
-      .then(res => {
-        console.log(res.data)
+  const deletedUserById = (id) => {
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .delete(URL)
+      .then((res) => {
+        console.log(res.data);
         // Actualiza la vista despue de borrar el usuaio
-        getAllUsers()
+        getAllUsers();
       })
-      .catch(err => console.log(err))
-
-  }
+      .catch((err) => console.log(err));
+  };
   // Funcion que Actualiza el usuario en la base de datos de la API (PATCH)
   const editUsersById = (id, data) => {
-    const URL = `${baseURL}/users/${id}/`
-    axios.patch(URL, data)
-      .then(res => {
-        console.log(res.data)
+    const URL = `${baseURL}/users/${id}/`;
+    axios
+      .patch(URL, data)
+      .then((res) => {
+        console.log(res.data);
         // Actualiza la vista despue de editar el usuaio
-        getAllUsers()
+        getAllUsers();
       })
-      .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   // Funcion para abrir formulario desde boton crear nuevo usuario
   const handleOpenForm = () => {
-    setUpdateInfo()
-    setCloseForm(false)
-  }
+    setUpdateInfo();
+    setCloseForm(false);
+  };
 
   return (
     <div className="App">
       <div className="app__container-ttbt">
-        <h1 className="App__title" >Users Crud</h1>
-        <button onClick={handleOpenForm} className='App__btn' >Create New User<i className="btn__add fa-solid fa-plus"></i></button>
+        <h1 className="App__title">Users Crud</h1>
+        <button onClick={handleOpenForm} className="App__btn">
+          Create New User<i className="btn__add fa-solid fa-plus"></i>
+        </button>
       </div>
-      <div className={`form__content ${closeForm && 'disable__form'}`}>
+      <div className={`form__content ${closeForm && "disable__form"}`}>
         <UsersForm
           createNewUsers={createNewUsers}
           updateInfo={updateInfo}
@@ -94,24 +96,27 @@ function App() {
           setUpdateInfo={setUpdateInfo}
           setCloseForm={setCloseForm}
         />
-      </div >
-      <div className="card__content">
-        {
-          loading ? <Loading /> :
-            users?.map(user => (
-              <UserCard
-                key={user.id}
-                user={user}
-                deletedUserById={deletedUserById}
-                setUpdateInfo={setUpdateInfo}
-                setCloseForm={setCloseForm}
-              />
-            ))
-        }
       </div>
-      <footer className='App__footer' ><p className='App__copywrite'>Creado con ❤ por Pako Cárdenas</p></footer>
+      <div className="card__content">
+        {loading ? (
+          <Loading />
+        ) : (
+          users?.map((user) => (
+            <UserCard
+              key={user.id}
+              user={user}
+              deletedUserById={deletedUserById}
+              setUpdateInfo={setUpdateInfo}
+              setCloseForm={setCloseForm}
+            />
+          ))
+        )}
+      </div>
+      <footer className="App__footer">
+        <p className="App__copywrite">Creado con ❤ por Pako Cárdenas</p>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -1,75 +1,121 @@
-import React, { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import '../assets/styles/userForm.css'
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import "../assets/styles/userForm.css";
 
 const defaulValues = {
-  email: '',
-  password: '',
-  first_name: '',
-  last_name: '',
-  birthday: '',
-}
+  email: "",
+  password: "",
+  first_name: "",
+  last_name: "",
+  birthday: "",
+};
 
-const UsersForm = ({ createNewUsers, updateInfo, editUsersById, setUpdateInfo, setCloseForm }) => {
-
+const UsersForm = ({
+  createNewUsers,
+  updateInfo,
+  editUsersById,
+  setUpdateInfo,
+  setCloseForm,
+}) => {
   //Uso de hook useForm para controlar la entrada te datos de form
-  const { handleSubmit, reset, register } = useForm()
+  const { handleSubmit, reset, register } = useForm();
 
   //Con este hook contolamos cuando el formulario edita (POST update)
   useEffect(() => {
     if (updateInfo) {
-      reset(updateInfo)
+      reset(updateInfo);
     }
-  }, [updateInfo])
+  }, [updateInfo]);
 
-  //Funcion para decirle al formulario si los datos ingresados son 
+  //Funcion para decirle al formulario si los datos ingresados son
   //spara crear un nuevo registro o editarlo
   //Si lo edita carga los datos de ese registro en el foormulario
-  const submit = data => {
-
+  const submit = (data) => {
     if (updateInfo) {
       //Editar usuario
-      editUsersById(updateInfo.id, data)
-      setUpdateInfo()
+      editUsersById(updateInfo.id, data);
+      setUpdateInfo();
     } else {
       //Crear usuario
-      createNewUsers(data)
+      createNewUsers(data);
     }
-    reset(defaulValues)
-    setCloseForm(true)
-  }
+    reset(defaulValues);
+    setCloseForm(true);
+  };
   // Funcion para cerrar formulario
   const handleCloseForm = () => {
-    setCloseForm(true)
-    reset(defaulValues)
-  }
+    setCloseForm(true);
+    reset(defaulValues);
+  };
   return (
-    <form className='form' onSubmit={handleSubmit(submit)}>
-      <i onClick={handleCloseForm} className="form__close fa-solid fa-circle-xmark"></i>
-      <h2 className='form__title'>{updateInfo ? 'Edit User' : 'New User'}</h2>
+    <form className="form" onSubmit={handleSubmit(submit)}>
+      <i
+        onClick={handleCloseForm}
+        className="form__close fa-solid fa-circle-xmark"
+      ></i>
+      <h2 className="form__title">{updateInfo ? "Edit User" : "New User"}</h2>
       <div className="form__element">
-        <label className="form__label" htmlFor="email">Email</label>
-        <input className="form__input" type="email" id="email" placeholder="Enter email"{...register("email")} />
+        <label className="form__label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="form__input"
+          type="email"
+          id="email"
+          placeholder="Enter email"
+          {...register("email")}
+        />
       </div>
       <div className="form__element">
-        <label className="form__label" htmlFor="password">Password</label>
-        <input className="form__input" type="password" id="password" placeholder="Enter password" {...register("password")} />
+        <label className="form__label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="form__input"
+          type="password"
+          id="password"
+          placeholder="Enter password"
+          {...register("password")}
+        />
       </div>
       <div className="form__element">
-        <label className="form__label" htmlFor="first_name">First Name</label>
-        <input className="form__input" type="text" id="first_name" placeholder="Enter first name" {...register("first_name")} />
+        <label className="form__label" htmlFor="first_name">
+          First Name
+        </label>
+        <input
+          className="form__input"
+          type="text"
+          id="first_name"
+          placeholder="Enter first name"
+          {...register("first_name")}
+        />
       </div>
       <div className="form__element">
-        <label className="form__label" htmlFor="last_name">Last Name</label>
-        <input className="form__input" type="text" id="last_name" placeholder="Enter last name" {...register("last_name")} />
+        <label className="form__label" htmlFor="last_name">
+          Last Name
+        </label>
+        <input
+          className="form__input"
+          type="text"
+          id="last_name"
+          placeholder="Enter last name"
+          {...register("last_name")}
+        />
       </div>
       <div className="form__element">
-        <label className="form__label" htmlFor="birthday">Birthday</label>
-        <input className="form__input" type="date" id="birthday" {...register("birthday")} />
+        <label className="form__label" htmlFor="birthday">
+          Birthday
+        </label>
+        <input
+          className="form__input"
+          type="date"
+          id="birthday"
+          {...register("birthday")}
+        />
       </div>
-      <button className='form__btn'>{updateInfo ? 'Edit' : 'Create'}</button>
+      <button className="form__btn">{updateInfo ? "Edit" : "Create"}</button>
     </form>
-  )
-}
+  );
+};
 
-export default UsersForm
+export default UsersForm;
